20f4fd2303d62d9f28be71c6a90d4d659e6dd4cc
TandRAF <grosu.rafael@mail.ru>
2022-11-19 16:41:38 +0200

    class button

--- homelabs/scripts/main.js
+++ homelabs/scripts/main.js
@@ -1,3 +1,5 @@
+//Nav-bar---------------------------------------------------------------------------------------------------------------------------
+
 const burger = document.querySelector(".burger"),
       nav = document.querySelector("nav");
 
@@ -5,3 +7,28 @@ burger.addEventListener('click', ()=>{
     console.log("hello")
     nav.classList.toggle("active");
 });
+
+//Classes buttons-------------------------------------------------------------------------------------------------------------------
+
+const classButtons = document.querySelectorAll(".classes");
+       
+const filterClass = n =>{
+    for(let classButton of classButtons){
+        classButton.classList.remove("active");
+    }
+    classButtons[n].classList.add("active");
+}
+
+classButtons.forEach((item, index) =>{
+    item.addEventListener("click", ()=>{
+        let active = index;
+        filterClass(active);
+        if(classButtons[active].classList.contains("active")){
+            classButtons[active].addEventListener("click", () =>{
+                console.log("hello")
+                classButtons[active].classList.toggle("active");
+            });
+        };
+    })
+})
+
